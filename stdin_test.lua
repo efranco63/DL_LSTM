b7a9038fd5f814f16a92dcdfb7f7709ad6732277
Eduardo Franco <ef1265@nyu.edu>
2015-04-21 13:45:56 -0400

    testing stdin and out

---
+++ stdin_test.lua
@@ -0,0 +1,36 @@
+ptb = require('data')
+stringx = require('pl.stringx')
+require 'io'
+
+function readline()
+  local line = io.read("*line")
+  if line == nil then error({code="EOF"}) end
+  line = stringx.split(line)
+  if tonumber(line[1]) == nil then error({code="init"}) end
+  for i = 2,#line do
+    -- check to see if the word is in the vocabulary
+    if not ptb.vocab_map[line[i]] then error({code="vocab", word = line[i]}) end
+  end
+  return line
+end
+
+while true do
+  print("Query: len word1 word2 etc")
+  local ok, line = pcall(readline)
+  if not ok then
+    if line.code == "EOF" then
+      break -- end loop
+    elseif line.code == "vocab" then
+      print("Word not in vocabulary: ", line.word)
+    elseif line.code == "init" then
+      print("Start with a number")
+    else
+      print(line)
+      print("Failed, try again")
+    end
+  else
+    print("Thanks, I will print now")
+    for i = 1, line[1] do io.write(line) end
+    io.write('\n')
+  end
+end
